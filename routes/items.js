9ea1ec6d8e3e22ba64e6ad987c0a8929ba764b02
Ksena <ksena@MacBook-Pro.local>
2021-02-02 15:49:59 +0200

    feat(one-app): adding post and delete routes

--- routes/items.js
+++ routes/items.js
@@ -5,16 +5,16 @@ const router = express.Router();
 
 const data = [
     {id: 1, title: 'Hello', order: 1},
-    {id: 1, title: 'World', order: 2}
+    {id: 2, title: 'World', order: 2}
 ];
 
 router.get('/', (req, res) => {
     res.status(200).json(data);
 });
 
-router.get('/:title', (req, res) => {
+router.get('/:id', (req, res) => {
     let found = data.find((item) => {
-           return item.title === parseInt(req.params.title)
+           return item.id === parseInt(req.params.id)
     })
     if(found) {
         res.status(200).json(found)
@@ -22,5 +22,45 @@ router.get('/:title', (req, res) => {
         res.status(404)
     }
 })
+router.post('/', function (req, res) {
+   
+    let itemIds = data.map(item => item.id);
+    
+    let orderNums = data.map(item => item.order);
+
+  
+    let newId = itemIds.length > 0 ? Math.max.apply(Math, itemIds) + 1 : 1;
+    
+    let newOrderNum = orderNums.length > 0 ? Math.max.apply(Math, orderNums) + 1 : 1;
+    let newItem = {
+        id: newId,
+        title: req.body.title, 
+        order: newOrderNum, 
+     
+    };
+
+   
+    data.push(newItem);
+
+    
+    res.status(201).json(newItem);
+});
+
+router.delete('/:id', function (req, res) {
+    
+    let found = data.find(function (item) {
+        return item.id === parseInt(req.params.id);
+    });
+
+    if (found) {
+       
+        let targetIndex = data.indexOf(found);
+
+       
+        data.splice(targetIndex, 1);
+    }
+
+    res.sendStatus(204);
+});
 
 module.exports = router
